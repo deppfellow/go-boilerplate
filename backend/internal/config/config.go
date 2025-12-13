a2e8de7651093edc24ebd618de540df37a945d7d
deppfellow <deppfellow@gmail.com>
2025-12-13 20:32:50 +0700

    chore: define config struct in config.go

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -5,3 +5,46 @@
 // validates that required values are present so they
 // can be reused accross the application runtime.
 package config
+
+/*
+	`koanf` is a config library. Its job is to read config source
+	(e.g `.env`, yaml, json, etc) then unmarshal (i.e. decode from lower-level
+	to higher-level obejct structure) into your struct.
+*/
+
+type Config struct {
+	Primary  Primary        `koanf:"primary" validate:"required"`
+	Server   ServerConfig   `koanf:"server" validate:"required"`
+	Database DatabaseConfig `koanf:"database" validate:"required"`
+	Auth     AuthConfig     `koanf:"auth" validate:"required"`
+}
+
+type Primary struct {
+	Env string `koanf:"env" validate:"required"`
+}
+
+type ServerConfig struct {
+	Port               string   `koanf:"port" validate:"required"`
+	ReadTimeout        int      `koanf:"read_timeout" validate:"required"`
+	WriteTimeout       int      `koanf:"write_timeout" validate:"required"`
+	IdleTimeout        int      `koanf:"idle_timeout" validate:"required"`
+	CORSAllowedOrigins []string `koanf:"cors_allowed_origins" validate:"required"`
+}
+
+type DatabaseConfig struct {
+	Host            string `koanf:"host" validate:"required"`
+	Port            int    `koanf:"port" validate:"required"`
+	User            string `koanf:"user" validate:"required"`
+	Password        string `koanf:"password" validate:"required"`
+	Name            string `koanf:"name" validate:"required"`
+	SSLMode         string `koanf:"ssl_mode" validate:"required"`
+	MaxOpenConns    int    `koanf:"max_open_conns" validate:"required"`
+	MaxIdleConns    int    `koanf:"max_idle_conns" validate:"required"`
+	ConnMaxLifetime int    `koanf:"conn_max_lifetime" validate:"required"`
+	ConnMaxIdleTime int    `koanf:"conn_max_idle_time" validate:"required"`
+}
+
+// Storing secret key variable of authentication
+type AuthConfig struct {
+	SecretKey string `koanf:"secret_key" validate:"required"`
+}
